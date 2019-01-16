96aa133136caf34e2c45fc28c69fb9bf24b078b4
prasannakumarbe <prasannakumarbe123@gmail.com>
2019-01-16 14:21:04 +0530

    first Pull

--- RMS/src/com/crg/rms/Adverties.java
+++ RMS/src/com/crg/rms/Adverties.java
@@ -1,5 +1,8 @@
  package com.crg.rms;
-
+/*
+*@ Class:Adverties
+*@ Version:1.0
+*/
 public class Adverties extends Employee {
 	String advName;
 	public Adverties(int empId, String empName, String jobType,String advName) {
